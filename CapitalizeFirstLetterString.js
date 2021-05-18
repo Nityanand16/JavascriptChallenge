ff6f7d679b808f8464f12f9675be071b2d113b36
Nityanand16 <nityanandgupta@yahoo.com>
2021-05-18 09:41:18 +0200

    code update

--- CapitalizeFirstLetterString.js
+++ CapitalizeFirstLetterString.js
@@ -1,38 +1,39 @@
 //program to count total number of characters in a string
 //program to capitalize the first character of a string
 
-console.log("********basics********")
-var a = "Nityanand"
+console.log("********basics********");
+var a = "Nityanand";
 console.log(a); //Nityanand
 
-var b = 10
+var b = 10;
 b++;
 console.log(b); //11
 
-var a = "Nityanand"
-console.log(a.length);  //9
+var a = "Nityanand";
+console.log(a.length); //9
 
 function boy(name) {
-    console.log("Hi    " + name)    //Hi Nityanand
+  console.log("Hi    " + name); //Hi Nityanand
 }
 boy("Nityanand");
 
 var d;
 console.log(d);
-console.log("********basics********")
+console.log("********basics********");
 
 //program to count total number of characters in a string
 function person(name) {
-    var length=0;
-    while (name[length]!==undefined)
-        length++;
-        return length;
+  var length = 0;
+  while (name[length] !== undefined) length++;
+  return length;
 }
-console.log(person("Nityanand"));   //9
+console.log(person("Nityanand")); //9
 
-console.log("********************************")
+console.log("********************************");
 
 //program to capitalize the first character of a string
 function capital(str) {
-    
+  return str.charAt(0).toUpperCase().str.slice(1);
 }
+console.log(capital("nityanand"));
+
